62d9ce0157ce108e088e3162ccadf8b64ba15c7f
Paulo <paulozamora2000@gmail.com>
2021-08-08 12:44:37 -0500

    Manejar errores en comando ls

--- OS/modules/commands.c
+++ OS/modules/commands.c
@@ -5,20 +5,28 @@
     0: Exitoso
     1: Directorio no encontrado
     2: Archivo no es un directorio
+    3: Faltan parámetros
 */
 int ls(char *outputBuffer, int argc, char argv[][TOKENLEN]){
   Dir foundFile;
   iNodo dirInode;
   Dir directory[64];
 
+  if(argc < 2){
+    strcpy(outputBuffer, "Too few parameters.\nUsage:\n  ls [path]");
+    return 3;
+  }
+
   foundFile = namei(argv[1]);
   if(foundFile.iNodo == -1){
+    strcpy(outputBuffer, "Directory not found");
     return 1;
   }
 
-
   getInode(&dirInode, foundFile.iNodo);
   if(dirInode.type != 'd'){
+    strcpy(outputBuffer, argv[1]);
+    strncat(outputBuffer, " is not a directory", 20);
     return 2;
   }
 
